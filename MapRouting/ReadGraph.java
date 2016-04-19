e3ca430f4523108d1ac37b105c2a711752f38897
TheCodeOfLife <ltbaum@live.unc.edu>
2016-04-19 10:12:26 -0400

    Removed Unnecessary Files

--- MapRouting/ReadGraph.java
+++ MapRouting/ReadGraph.java
@@ -15,10 +15,10 @@ public class ReadGraph {
 		File file = new File("/Users/ltbaum/Documents/workspace/Optimize Dijkstra/src/usa.txt");
 
 		Scanner scan = new Scanner(file);
-
+		//scan in num vertices and edges
 		int numVertices = scan.nextInt();
 		int numEdges = scan.nextInt();
-
+		//for each indicated vertex and edge, read in next
 		for(int i=0; i<numVertices; i++){
 			GraphNode vertex = new GraphNode(scan.nextInt(),scan.nextInt(), scan.nextInt());
 			graph.put(vertex.getName(), vertex);
@@ -31,15 +31,27 @@ public class ReadGraph {
 			graph.get(v2).addEdge(graph.get(v1));
 		}
 		
+		//determine source and destination
 		GraphNode source = graph.get(0);
 		GraphNode destination = graph.get(5000);
+
+		Scanner user = new Scanner(System.in);
 		
-		//MinHeap<GraphNode> heap = new MinHeap<GraphNode>();
-		
-		long startTime = System.currentTimeMillis();
-		System.out.println(MaybeDijkstras(source, destination, graph));
-		long endTime = System.currentTimeMillis();
-		System.out.println("Dijkstras computed in "+(endTime-startTime) +" milisecs");
+		System.out.println("Type \"Dijkstras\" for basic, or \"better\" for the improved one.");
+		String next = user.nextLine();
+		if(next.equals("Dijkstras")){
+			long startTime = System.currentTimeMillis();
+			System.out.println(Dijkstras(source, destination, graph));
+			long endTime = System.currentTimeMillis();
+			System.out.println("Dijkstras computed in "+(endTime-startTime) +" milisecs");
+		}		
+		else if (next.equals("better")){
+			long startTime = System.currentTimeMillis();
+			System.out.println(BetterDijkstras(source, destination, graph));
+			long endTime = System.currentTimeMillis();
+			System.out.println("Modified/Improved Dijkstras computed in "+(endTime-startTime) +" milisecs");
+		}
+		else System.out.println("Retry");
 		
 
 	}
@@ -66,7 +78,7 @@ public class ReadGraph {
 		return destination.getDistance();
 	}
 	
-	private static double MaybeDijkstras(GraphNode source, GraphNode destination, HashMap<Integer, GraphNode> graph2) {
+	private static double BetterDijkstras(GraphNode source, GraphNode destination, HashMap<Integer, GraphNode> graph2) {
 		//PriorityQueue<GraphNode> queue = new PriorityQueue<GraphNode>();
 		MinHeap<GraphNode> heap = new MinHeap<GraphNode>();
 	
@@ -82,26 +94,6 @@ public class ReadGraph {
 					e.setParent(u);
 					heap.add(e);
 				}
-				//if(e==destination) return e.getDistance();
-			}
-		}
-		
-		return destination.getDistance();
-	}
-	
-	private static double BetterDijkstras(GraphNode source, GraphNode destination, HashMap<Integer, GraphNode> graph2) {
-		PriorityQueue<GraphNode> queue = new PriorityQueue<GraphNode>(graph2.values());
-	
-		graph2.get(source.getName()).setDistance(0);
-
-		while(!queue.isEmpty()){
-			GraphNode u = queue.poll();
-			for(GraphNode e: u.getEdges()){
-				if((u.distanceTo(e)+u.getDistance())<e.getDistance()){
-					e.setDistance(u.getDistance()+u.distanceTo(e));
-					e.setParent(u);
-					if(e==destination) return e.getDistance();
-				}
 				if(e==destination) return e.getDistance();
 			}
 		}
@@ -109,27 +101,4 @@ public class ReadGraph {
 		return destination.getDistance();
 	}
 	
-	
-	
-//	private static double BetterDijkstras(GraphNode source, GraphNode destination, HashMap<Integer, GraphNode> graph2) {
-//		//PriorityQueue<GraphNode> queue = new PriorityQueue<GraphNode>(graph2.values());
-//		FibonacciHeap<GraphNode> heap = new FibonacciHeap<GraphNode>();
-//
-//		graph2.get(source.getName()).setDistance(0);
-//		for(int e: graph2.keySet()){
-//			heap.enqueue(graph2.get(e), graph2.get(e).getDistance());
-//		}
-//
-//		while(!heap.isEmpty()){
-//			GraphNode u = heap.dequeueMin().getValue();
-//			for(GraphNode e: u.getEdges()){
-//				if((u.distanceTo(e)+u.getDistance())<e.getDistance()){
-//					e.setDistance(u.getDistance()+u.distanceTo(e));
-//					e.setParent(u);
-//				}
-//			}
-//		}
-//		
-//		return destination.getDistance();
-//	}
 }
